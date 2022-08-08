a418da3550f2e1fd30a595f2414dcc05c8a040e8
Ihogan25 <isaiah.hogan25@gmail.com>
2022-08-07 22:41:28 -0500

    final commit

--- app.js
+++ app.js
@@ -1,75 +1,62 @@
-//buttons and choices
-let humanRock = document.getElementById('rock');
-let humanPaper = document.getElementById('paper');
-let humanScissors = document.getElementById('scissors');
-let resetBtn = document.getElementById('reset');
-let computer = document.getElementById('computer-choice')
-let humanChoice = document.getElementById('human-choice')
-
-//scores
-let final = document.getElementById('final');
-let humanScore = document.getElementById('human-score');
-let computerScore = document.getElementById('computer-score');
-
-//  computer choice //
-const computerChoice = function() {
-    let choice = '';
-    const choices = ['Computer Choice: Rock', 'Computer Choice: Paper', 'Computer Choice: Scissors'];
-    let num = Math.floor(Math.random() * 3);
-    choice = choices[num];
-    return choice;
-}
-let scoreOne = 0;
-let scoreTwo = 0;
-const toWin = 5;
-
-const game  = function () {
-    
-    
-if  (humanChoice.textContent !== 'Human Choice: Rock') {
-
-        humanRock.addEventListener('click', function(){
-
-        humanChoice.textContent = 'Human Choice: Rock';
-
-        computer.textContent = computerChoice();
-
-})}; 
-
-if(humanChoice.textContent !== 'Human Choice: Paper') {
-        humanPaper.addEventListener('click', function(){
-
-        humanChoice.textContent = 'Human Choice: Paper';
-
-        computer.textContent = computerChoice();
-
-})}; 
-
-if(humanChoice.textContent !== 'Human Choice: Scissors')
-
-        humanScissors.addEventListener('click', function(){
-
-        humanChoice.textContent = 'Human Choice: Scissors';
-
-        computer.textContent = computerChoice();
+//variable slectors
+const options = document.querySelectorAll(".options")
+const body = document.querySelector('body')
+const human = document.querySelector('.human');
+const computer = document.querySelector('.computer');
+const winner = document.querySelector('.winner')
+const compChoice = document.querySelector('.compChoice')
+const round= document.querySelector('.round')
 
 
-    });
-
+//computer slection
+function computerChoice() {
+        const choices = ['Rock', 'Paper', 'Scissors'];
+        compChoice.textContent = choices[Math.floor(Math.random() * 3)];
 };
+//round and scores
+let computerScore = 0;
+let humanScore = 0;
+let roundNum = 0;
 
+//Function for each Option
+options.forEach(option=> {
+        option.addEventListener('click', function() {
+                //run the game if round is less than 5
+               if(roundNum < 5) {
+                        //each time a human choice is selected call computerchoice
+                        computerChoice()
+                        //then if block runs to compare human to computer choice
+                        if(option.textContent === compChoice.textContent) {
+                                console.log('tie')
+                        } else if(
+                                (option.textContent === 'Rock' && compChoice.textContent === 'Scissors') ||
+                                (option.textContent === 'Paper' && compChoice.textContent === 'Rock') ||
+                                (option.textContent === 'Scissors' && compChoice.textContent === 'Paper')) 
+                                {
+                                        console.log('Human')
+                                        humanScore++
+                                        human.textContent = `Human: ${humanScore}`
+                        } else {          
+                                console.log('Computer')
+                                computerScore++;
+                                computer.textContent = `Computer: ${computerScore}`
+                        }
+                //inscrase round each time human clicks an option         
+               roundNum++;
+               round.textContent = `Round: ${roundNum}`;
 
-
-game();
-
-
-
-
-
-
-
-
-
+                //only runs when round limit is reached
+                } else if(roundNum === 5) {
+                        if(humanScore === computerScore) {
+                                winner.textContent = 'Result: Draw'
+                        } else if(humanScore > computerScore) {
+                                winner.textContent = 'Result: Humans Win!'
+                        } else if(humanScore < computerScore) {
+                                winner.textContent = 'Result: Computers Win!'
+                        }
+                }
+        })
+})
 
 
 
